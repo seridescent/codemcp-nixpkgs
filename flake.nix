0973d23665548a3835c77b9c89bde2574e2a7972
seridescent <seridescent@gmail.com>
2025-05-22 17:12:42 -0400

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Quick and dirty Nix package definition for codemcp";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.callPackage ./package.nix { };
+          codemcp = pkgs.callPackage ./package.nix { };
+        };
+      });
+}
